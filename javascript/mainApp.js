f229765b5155027440c02a433d0c4dc3ed9e8531
ErwinBrgr <t.chaperon@gmail.com>
2019-02-13 21:41:28 +0100

    passsage var mymap dans obj

--- javascript/mainApp.js
+++ javascript/mainApp.js
@@ -13,6 +13,14 @@ orangeIcon : './images/orange_icon.png',
 
 recupPoints : function() {
 
+var mymap = L.map('mapid').setView([MapObj.latitude, MapObj.longitude], MapObj.zoom);
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoiZXJ3aW5icmdyIiwiYSI6ImNqbnQ1czl6cTBvYWEzcHBqOXBkMDB2bzcifQ.YfJ2wPJux8VPSoLBbXMgLA'
+}).addTo(mymap);
+
 	ajaxGet(this.urlapi, function (reponse) {
     // Transforme la réponse en un tableau d'articles
     var stations = JSON.parse(reponse);
@@ -119,16 +127,6 @@ mymap.addLayer(markers);
 
 
 MapObj.recupPoints();
-
-
-var mymap = L.map('mapid').setView([MapObj.latitude, MapObj.longitude], MapObj.zoom);
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox.streets',
-    accessToken: 'pk.eyJ1IjoiZXJ3aW5icmdyIiwiYSI6ImNqbnQ1czl6cTBvYWEzcHBqOXBkMDB2bzcifQ.YfJ2wPJux8VPSoLBbXMgLA'
-}).addTo(mymap);
-
 
 valid = function () {
             sessionStorage.setItem("nomStation", $("#nomStation").html());
