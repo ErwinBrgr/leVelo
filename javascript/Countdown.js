9342fef2555e1e4c1e1c41bd0da06a4472ee177f
ErwinBrgr <t.chaperon@gmail.com>
2019-03-06 07:44:19 +0100

    Correction avant soutenance

--- javascript/Countdown.js
+++ javascript/Countdown.js
@@ -7,21 +7,28 @@
 var CountDownObj  = {
 	//properties
 	countDownDate : 0,
-
+	distance:0,
+	interval : null,
 	//methods
 //Méthode permettant de la définition de la distance étant la différence entre la date du jour et l'instant T
 	
 	ct : function(){
-		sessionStorage.setItem("distance", CountDownObj.distance);
+		if (sessionStorage.getItem("nomStation")!==null) {
+			sessionStorage.setItem("distance", CountDownObj.distance);
+		}else{
+			sessionStorage.removeItem("distance");
+			clearInterval(CountDownObj.interval);
+		}
+		
 		var now = new Date().getTime(); //stockage de la date du jour en variable
 		CountDownObj.distance = CountDownObj.countDownDate  - now ;//définition de la distance
-		CountDownObj.minutes = Math.floor((CountDownObj.distance % (1000 * 60 * 60)) / (1000 * 60)); //conversion des millisecond en minutes
-		CountDownObj.seconds = Math.floor((CountDownObj.distance % (1000 * 60)) / 1000);//conversion en secondes
-		document.getElementById("timer").innerHTML = CountDownObj.minutes + "m " + CountDownObj.seconds + "s "; //affichage minutes et secondes
+		var minutes = Math.floor((CountDownObj.distance % (1000 * 60 * 60)) / (1000 * 60)); //conversion des millisecond en minutes
+		var seconds = Math.floor((CountDownObj.distance % (1000 * 60)) / 1000);//conversion en secondes
+		document.getElementById("timer").innerHTML = minutes + "m " + seconds + "s "; //affichage minutes et secondes
 		
 
 		if (CountDownObj.distance < 0) {
-            clearInterval(x);
+            clearInterval(CountDownObj.interval);
             sessionStorage.clear();
             document.getElementById("timer").innerHTML = "Réservation non valide"; //gestion en cas de session expiré
         }
@@ -29,14 +36,19 @@ var CountDownObj  = {
 	},
 //cette méthode initialise le timer avec un interval de 1 secs (1000 millisecondes)
 	timer : function(distance = 1200000 ){ //mise en parametre de 20 mins en millisecondes d
+		CountDownObj.distance = distance;
 		CountDownObj.countDownDate = new Date().getTime() + parseInt(distance); // date du jour + 20mins 
-		x = setInterval(CountDownObj.ct,1000);//définition de l'interval 1000 millisecondes pour 1sec
+		setInterval(CountDownObj.ct,1000);//définition de l'interval 1000 millisecondes pour 1sec
 
+	},
+
+	clearInt : function(){
+		 clearInterval(this.interval);
+		 
 	}
 };
 
 //déclanchement du timer au clic
 $('#valid').on('click', function(){
     CountDownObj.timer();
-    CountDownObj.minutes = sessionStorage.getItem('distance');
 });
